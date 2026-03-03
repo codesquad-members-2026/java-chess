28d9a860417e6de4e5c43d58ab4c7d38fbde4624
LeeJaeWan <jjjkuul@naver.com>
2026-03-03 16:14:26 +0900

    ** 1단계: 체스 프로젝트 시작
    *** Pawn 클래스를 구현 -> 멤버 변수로 color를 가지고, getColor() 메서드를 통해 color의 값을 반환받을 수 있음
    *** PawnTest 클래스를 구현 -> Pawn 객체 생성 시 멤버 변수 color에 대입되는 값이 입력된 값과 동일한지 테스트

---
+++ app/src/main/java/com/codesquad/chess/Pawn.java
@@ -0,0 +1,13 @@
+package com.codesquad.chess;
+
+public class Pawn {
+    private String color;
+
+    Pawn(String color){
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
