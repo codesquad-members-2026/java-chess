10bab571a8432877fb7c4164481b3c78438ab5be
LeeJaeWan <jjjkuul@naver.com>
2026-03-04 12:22:00 +0900

    BoardTest 클래스 중복 제거 - @BeforeEach 사용 / toBoardAddSizeFind() 메서드 추가

--- app/src/test/java/com/codesquad/chess/BoardTest.java
+++ app/src/test/java/com/codesquad/chess/BoardTest.java
@@ -1,6 +1,7 @@
 package com.codesquad.chess;
 
 import com.codesquad.chess.piece.Pawn;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,22 +9,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTest {
 
+    private Board board;
+
+    // @Test 어노테이션이 붙은 메서드들 실행 전 실행
+    @BeforeEach
+    public void setUp(){
+        board = new Board();
+        System.out.println("보드 초기화 완료");
+    }
+
     @Test
     @DisplayName("말이 올라올 수 있는 보드판")
-    public void create(){
-        Board board = new Board();
-
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
-
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
+    public void testPawnsOnTheBoard(){
+        toBoardAddSizeFind(new Pawn(Pawn.WHITE_COLOR), 1, 0);
+        toBoardAddSizeFind(new Pawn(Pawn.BLACK_COLOR), 2, 1);
 
         // Pawn 이외의 다른 객체 추가시 오류 발생
         // board.add(new Integer(7));
+    }
+
+    private void toBoardAddSizeFind(Pawn pawn, int expectedSize, int expectedIndex){
+        board.add(pawn);
+        assertEquals(expectedSize, board.size());
+        assertEquals(pawn, board.findPawn(expectedIndex));
     }
 }
