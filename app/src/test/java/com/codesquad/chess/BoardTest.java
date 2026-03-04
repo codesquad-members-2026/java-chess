e2d43e1919e1760918686b794faf1f6800d2738a
Wonjae Song <stojum@naver.com>
2026-03-04 11:58:52 +0900

    상수 추가

--- app/src/test/java/com/codesquad/chess/BoardTest.java
+++ app/src/test/java/com/codesquad/chess/BoardTest.java
@@ -17,12 +17,12 @@ public class BoardTest {
     @Test
     public void create() throws Exception {
 
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
+        Pawn white = new Pawn(Pawn.WHITE_COLOR,Pawn.WHITE_REPRESENTATION);
         board.add(white);
         assertEquals(1, board.size());
         assertEquals(white, board.findPawn(0));
 
-        Pawn black = new Pawn(Pawn.BLACK_COLOR);
+        Pawn black = new Pawn(Pawn.BLACK_COLOR,Pawn.BLACK_REPRESENTATION);
         board.add(black);
         assertEquals(2, board.size());
         assertEquals(black, board.findPawn(1));
