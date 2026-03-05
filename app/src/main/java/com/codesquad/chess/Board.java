a3d8c0f0bb6ea686c03da68ec82c7aef6c98b770
Wonjae Song <stojum@naver.com>
2026-03-05 11:01:30 +0900

    오류 수정 진행

--- app/src/main/java/com/codesquad/chess/Board.java
+++ app/src/main/java/com/codesquad/chess/Board.java
@@ -1,67 +1,51 @@
 package com.codesquad.chess;
 
-import pieces.Pawn;
-
 import java.util.ArrayList;
 import java.util.List;
+import pieces.Pawn;
 
 public class Board {
 
     private List<Pawn> whitePawns = new ArrayList<>();
     private List<Pawn> blackPawns = new ArrayList<>();
 
-    public void addWhite(Pawn pawn) {
-        whitePawns.add(pawn);
-    }
-
-    public void addBlack(Pawn pawn) {
-        blackPawns.add(pawn);
-    }
-
-    public int size() {
-        return whitePawns.size() + blackPawns.size();
-    }
-
-    public Pawn findPawn(int index) {
-        return whitePawns.get(index);
-    }
-
     public void initialize() {
-        for(int i = 0; i < 8; i++) {
-            addWhite(new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION));
-            addBlack(new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION));
+        whitePawns.clear();
+        blackPawns.clear();
+
+        for (int i = 0; i < 8; i++) {
+            whitePawns.add(new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION));
+            blackPawns.add(new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION));
         }
-    }
-
-    public String getWhitePawnsResult() {
-        return getPawnsResult(whitePawns);
-    }
-
-    public String getBlackPawnsResult() {
-        return getPawnsResult(blackPawns);
-    }
-
-    private String getPawnsResult(List<Pawn> pawns) {
-        StringBuilder sb = new StringBuilder();
-        for(Pawn pawn : pawns) {
-            sb.append(pawn.getRepresentation());
-        }
-        return sb.toString();
     }
 
     public String showBoard() {
         StringBuilder sb = new StringBuilder();
         String blankLine = "........";
+        String newLine = System.getProperty("line.separator");
 
-        sb.append(blankLine).append("\n");
-        sb.append(getBlackPawnsResult()).append("\n");
-        sb.append(blankLine).append("\n");
-        sb.append(blankLine).append("\n");
-        sb.append(blankLine).append("\n");
-        sb.append(blankLine).append("\n");
-        sb.append(getWhitePawnsResult()).append("\n");
-        sb.append(blankLine).append("\n");
+        sb.append(blankLine).append(newLine);
+        sb.append(blankLine).append(newLine).append(blankLine).append(newLine);
+        sb.append(blankLine).append(newLine).append(blankLine).append(newLine);
+        sb.append(getWhitePawnsResult()).append(newLine);
+        sb.append(blankLine).append(newLine);
 
+        return sb.toString();
+    }
+
+    private String getWhitePawnsResult() {
+        return getPawnsResult(whitePawns);
+    }
+
+    private String getBlackPawnsResult() {
+        return getPawnsResult(blackPawns);
+    }
+
+    private String getPawnsResult(List<Pawn> pawns) {
+        StringBuilder sb = new StringBuilder();
+        for (Pawn pawn : pawns) {
+            sb.append(pawn.getRepresentation());
+        }
         return sb.toString();
     }
 }
--- app/src/main/java/pieces/Pawn.java
+++ app/src/main/java/pieces/Pawn.java
@@ -1,25 +1,21 @@
 package pieces;
 
 public class Pawn {
-
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
-
+    // 3단계 추가: 출력용 문자 상수
     public static final char WHITE_REPRESENTATION = 'p';
     public static final char BLACK_REPRESENTATION = 'P';
 
-
-    private final String color;
-    private final char representation;
+    private String color;
+    private char representation;
 
 
     public Pawn() {
-        this(WHITE_COLOR,WHITE_REPRESENTATION);
-
+        this(WHITE_COLOR, WHITE_REPRESENTATION);
     }
 
     public Pawn(String color, char representation) {
-
         this.color = color;
         this.representation = representation;
     }
@@ -31,5 +27,4 @@ public class Pawn {
     public char getRepresentation() {
         return representation;
     }
-
 }
