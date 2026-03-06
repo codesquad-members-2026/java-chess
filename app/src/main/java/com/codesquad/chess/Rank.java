ad1cb28cb4c81d34068cbf27a6d461d05c453e59
Wonjae Song <stojum@naver.com>
2026-03-06 12:19:01 +0900

    Rank 클래스 구현 보드 자료구조 개선

---
+++ app/src/main/java/com/codesquad/chess/Rank.java
@@ -0,0 +1,32 @@
+package com.codesquad.chess;
+
+import com.codesquad.chess.pieces.Piece;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rank {
+    private List<Piece> pieces = new ArrayList<>();
+
+    public void addPiece(Piece piece) {
+        pieces.add(piece);
+    }
+
+    public String getRankRepresentation() {
+        StringBuilder sb = new StringBuilder();
+        for(Piece piece : pieces) {
+            sb.append(piece.getRepresentation());
+        }
+        return sb.toString();
+    }
+
+    public int getPieceCount() {
+        int count = 0;
+        for (Piece piece : pieces) {
+            if (piece.getType() != Piece.Type.NO_PIECE) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
