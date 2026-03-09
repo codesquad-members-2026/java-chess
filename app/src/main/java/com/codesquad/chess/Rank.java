8cf82765900809e0df5f83c22a2c1b757258989a
Wonjae Song <stojum@naver.com>
2026-03-09 19:17:41 +0900

    Rank 도입을 위한 Rank.java 구현

---
+++ app/src/main/java/com/codesquad/chess/Rank.java
@@ -0,0 +1,21 @@
+package com.codesquad.chess;
+
+import com.codesquad.chess.pieces.Piece;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rank {
+    private List<Piece> pieces = new ArrayList<>();
+
+    public void addPiece(Piece piece) { pieces.add(piece); }
+    public void setPiece(int xPos, Piece piece) { pieces.set(xPos, piece); }
+    public Piece getPiece(int xPos) { return pieces.get(xPos); }
+
+    public String getRankRepresentation() {
+        StringBuilder sb = new StringBuilder();
+        for (Piece piece : pieces) {
+            sb.append(piece.getRepresentation());
+        }
+        return sb.toString();
+    }
+}
