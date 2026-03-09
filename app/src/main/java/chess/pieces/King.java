d430c6e8fef3ccb7b8d74b0756dcd76bce78e9a4
WOWND <jjp910123@gmail.com>
2026-03-09 17:11:13 +0900

    움직임 리팩토링

--- app/src/main/java/chess/pieces/King.java
+++ app/src/main/java/chess/pieces/King.java
@@ -8,31 +8,11 @@ import java.util.List;
 import java.util.Set;
 
 public class King extends Piece{
-    private static final List<Direction> directions = Direction.everyDirection();
-
     protected King(Type type, Color color) {
         super(type, color);
     }
     @Override
     public Set<Position> getValidMoves(Position from, Board board) {
-        Set<Position> validMoves = new HashSet<>();
-
-        for (Direction direction : directions) {
-            int newRank = from.rank + direction.getRankDelta();
-            int newFile = from.file + direction.getFileDelta();
-
-            if (newRank < 0 || newRank >= Board.SIZE || newFile < 0 || newFile >= Board.SIZE) {
-                continue;
-            }
-
-            Position to = new Position(newRank, newFile);
-            Piece piece = board.findPiece(to);
-            if (piece.getColor() == this.color) {
-                continue;
-            }
-
-            validMoves.add(new Position(newRank, newFile));
-        }
-        return validMoves;
+        return steppingMoves(from, board, Direction.everyDirection());
     }
 }
--- app/src/main/java/chess/pieces/Piece.java
+++ app/src/main/java/chess/pieces/Piece.java
@@ -1,8 +1,11 @@
 package chess.pieces;
 
 import chess.Board;
+import chess.Direction;
 import chess.Position;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.List;
 
 public abstract class Piece {
     public static Piece create(Type type, Color color) {
@@ -34,4 +37,55 @@ public abstract class Piece {
     }
 
     public abstract Set<Position> getValidMoves(Position from, Board board);
+
+    protected Set<Position> slidingMoves(Position from, Board board, List<Direction> directions) {
+        Set<Position> validMoves = new HashSet<>();
+
+        for (Direction direction : directions) {
+            int newRank = from.rank + direction.getRankDelta();
+            int newFile = from.file + direction.getFileDelta();
+
+            if (newRank < 0 || newRank >= Board.SIZE || newFile < 0 || newFile >= Board.SIZE) {
+                continue;
+            }
+
+            Position to = new Position(newRank, newFile);
+            Piece piece = board.findPiece(to);
+            if (piece.getColor() == this.color) {
+                continue;
+            }
+
+            validMoves.add(new Position(newRank, newFile));
+        }
+        return validMoves;
+    }
+
+    protected Set<Position> steppingMoves(Position from, Board board, List<Direction> directions) {
+        Set<Position> validMoves = new HashSet<>();
+
+        for (Direction direction : directions) {
+            for (int dist = 1; dist <= Board.SIZE; dist++) {
+                int newRank = from.rank + direction.getRankDelta() * dist;
+                int newFile = from.file + direction.getFileDelta() * dist;
+
+                if (newRank < 0 || newRank >= Board.SIZE || newFile < 0 || newFile >= Board.SIZE) {
+                    break;
+                }
+
+                Position to = new Position(newRank, newFile);
+                Piece piece = board.findPiece(to);
+
+                if (piece.getColor() == this.color) { // 아군
+                    break;
+                }
+
+                validMoves.add(new Position(newRank, newFile));
+
+                if (piece.getType() != Type.NO_PIECE) { // 진행 불가
+                    break;
+                }
+            }
+        }
+        return validMoves;
+    }
 }
