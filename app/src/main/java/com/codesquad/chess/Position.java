0f71cc6da366606c2a8f0edce914754e1a12624e
Wonjae Song <stojum@naver.com>
2026-03-09 19:18:06 +0900

    기물 좌표를 위한 Position.java 구현

---
+++ app/src/main/java/com/codesquad/chess/Position.java
@@ -0,0 +1,14 @@
+package com.codesquad.chess;
+
+public class Position {
+    private final int x;
+    private final int y;
+
+    public Position(String position) {
+        this.x = position.charAt(0) - 'a';
+        this.y = Board.SIZE - Character.getNumericValue(position.charAt(1));
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+}
