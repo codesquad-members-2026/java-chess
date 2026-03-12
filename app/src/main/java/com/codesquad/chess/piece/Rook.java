281059bd94a8bc8e411855926023623b66cc5a87
LeeJaeWan <jjjkuul@naver.com>
2026-03-12 14:22:56 +0900

    Rook 움직임 테스트 구현
    - Rook 또한 Queen의 대각선 검사 메서드만을 제외하고 똑같은 로직으로 테스트와 프로덕션 코드를 구현
    - 이후 중간 추상 메서드를 도입하여 세 객체의 유사한 중복 로직을 제거할 예정

--- app/src/main/java/com/codesquad/chess/piece/Rook.java
+++ app/src/main/java/com/codesquad/chess/piece/Rook.java
@@ -1,7 +1,11 @@
 package com.codesquad.chess.piece;
 
 import com.codesquad.chess.Board;
+import com.codesquad.chess.Direction;
 import com.codesquad.chess.Position;
+import com.codesquad.chess.exception.UnreachableDirectionException;
+
+import static com.codesquad.chess.utils.StringUtils.appendNewLine;
 
 public class Rook extends Piece {
 
@@ -11,7 +15,53 @@ public class Rook extends Piece {
 
     @Override
     public boolean verifyMovePosition(Position target, Board board){
+        Position source = this.getPosition();
 
-        return true;
+        if(!verifyStraight(source, target)){
+            return false;
+        }
+
+        Direction direction = findDirection(source, target);
+
+        return recurTarget(source, target, direction, board);
+    }
+
+    private boolean recurTarget(Position source, Position target, Direction direction, Board board){
+        // 종료 조건
+        if(isEqual(source, target)){
+            return true;
+        }
+
+        int nextX = source.getX() + direction.getXDegree();
+        int nextY = source.getY() - direction.getYDegree();
+        Position nextPosition = Position.of(nextX, nextY);
+
+        if(!board.findPiece(nextPosition).getType().equals(Type.NO_PIECE))
+            return false;
+
+        return recurTarget(nextPosition, target, direction, board);
+    }
+
+    private Direction findDirection(Position source, Position target){
+        int xSignum = Integer.signum(target.getX() - source.getX());
+        // y 좌표는 도메인적으로 [0]을 8번째 Rank, [7]을 첫 번째 Rank로 인지한다.
+        int ySignum = Integer.signum(source.getY() - target.getY());
+
+        for(Direction direction : Direction.everyDirection()){
+            if(direction.getXDegree() == xSignum && direction.getYDegree() == ySignum){
+                return direction;
+            }
+        }
+
+        throw new UnreachableDirectionException(
+                appendNewLine("출발지와 도착지로 방향을 계산할 수 없습니다.") +
+                        appendNewLine("source: [" + source.getX() + ", " + source.getY() + "]") +
+                        appendNewLine("target: [" + target.getX() + ", " + target.getY() + "]"));
+    }
+    private boolean verifyStraight(Position source, Position target){
+        return source.getX() == target.getX() || source.getY() == target.getY();
+    }
+    private boolean isEqual(Position source, Position target){
+        return source.getX() == target.getX() && source.getY() == target.getY();
     }
 }
