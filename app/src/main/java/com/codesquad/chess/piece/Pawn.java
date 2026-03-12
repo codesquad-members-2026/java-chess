ca831bb21343742aa07af4b7844d0e79e9cf8b63
LeeJaeWan <jjjkuul@naver.com>
2026-03-12 16:06:32 +0900

    흰색 Pawn 이동 로직 구현(1차)
    - 흰색 폰이 처음 이동할 때와 첫 이동 뒤에 이동할 때의 코드를 구현
    - 처음 이동할 때 한 칸 혹은 두 칸 이동할 수 있다.
    - 이를 구현하기 위해 Pawn 객체에 boolean 타입의 wasMoved 선언
    - target과 현재 위치의 Position x, y 값의 빼기 연산을 통해 Direction의 whitePawnDirection 메서드가 반환하는 상수들에 속하는지 검증
    - 한칸 혹은 대각 전진에 해당하지 않으면 이후 두 칸 전진인지 확인하는 로직 수행

--- app/src/main/java/com/codesquad/chess/piece/Pawn.java
+++ app/src/main/java/com/codesquad/chess/piece/Pawn.java
@@ -1,17 +1,40 @@
 package com.codesquad.chess.piece;
 
 import com.codesquad.chess.Board;
+import com.codesquad.chess.Direction;
 import com.codesquad.chess.Position;
 
 public class Pawn extends Piece {
 
+    private boolean wasMoved;
+    public static int ONE_STEP_X_VAL = 0;
+    public static int TWO_STEP_Y_VAL = 2;
+    public static int WHITE_PAWN_START_Y_IDX = 6;
+
     public Pawn(Piece.Color color, char representation, double point, Position position){
         super(color, representation, point, position);
+        wasMoved = false;
     }
 
     @Override
     public boolean verifyMovePosition(Position target, Board board){
+        int xMinus = this.getPosition().getX() - target.getX();
+        int yMinus = this.getPosition().getY() - target.getY();
 
-        return true;
+        // 한 칸 전진 혹은 대각 전진 확인 --> 더 세분화 필요
+        for(Direction d : Direction.whitePawnDirection()){
+            if(d.getXDegree() == xMinus && d.getYDegree() == yMinus){
+                return true;
+            }
+        }
+        
+        // 두 칸 전진 로직 확인
+        if(xMinus == ONE_STEP_X_VAL && yMinus == TWO_STEP_Y_VAL
+                && !wasMoved && this.getPosition().getY() == WHITE_PAWN_START_Y_IDX){
+            wasMoved = true;
+            return true;
+        }
+
+        return false;
     }
 }
--- app/src/test/java/com/codesquad/chess/piece/PawnTest.java
+++ app/src/test/java/com/codesquad/chess/piece/PawnTest.java
@@ -1,4 +1,47 @@
 package com.codesquad.chess.piece;
 
+import com.codesquad.chess.Board;
+import com.codesquad.chess.ChessView;
+import com.codesquad.chess.Position;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PawnTest {
+
+    private Board board;
+
+    @BeforeEach
+    public void setUp() {
+        board = new Board();
+    }
+
+    @Test
+    @DisplayName("흰색폰은 처음 한 칸 혹은 두 칸 북쪽으로 전진할 수 있다.")
+    public void verifyMovePosition_At_First_WhitePawn_Can_Move_OneOrTwoSpace(){
+        board.initializeEmptyBoard();
+
+        Piece whitePawn1 = Piece.createWhitePawn(Position.of("d2"));
+        board.setPiece(Position.of("d2"), whitePawn1);
+        Piece whitePawn2 = Piece.createWhitePawn(Position.of("e2"));
+        board.setPiece(Position.of("e2"), whitePawn2);
+        System.out.println(ChessView.showBoard(board.getRanks()));
+
+
+        // 첫 이동 시 한칸 혹은 두 칸 이동할 수 있음
+        board.move("d2", "d3"); // 한 칸 이동
+        board.move("e2", "e4"); // 두 칸 이동
+        assertEquals(whitePawn1, board.findPiece(Position.of("d3")));
+        assertEquals(whitePawn2, board.findPiece(Position.of("e4")));
+        System.out.println(ChessView.showBoard(board.getRanks()));
+
+        // 첫 이동 후 한 칸만 전진할 수 있다.
+        board.move("d3", "d4"); // 한 칸 이동은 가능
+        board.move("e4", "e6"); // 두 칸 이동은 불가
+        assertEquals(whitePawn1, board.findPiece(Position.of("d4")));
+        assertEquals(whitePawn2, board.findPiece(Position.of("e4"))); // 이동이 거부된 상태
+        System.out.println(ChessView.showBoard(board.getRanks()));
+    }
 }
