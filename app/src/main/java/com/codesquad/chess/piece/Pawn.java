3023d176130fb8a9aacd14378600ae6eacb16cb1
LeeJaeWan <jjjkuul@naver.com>
2026-03-12 21:00:38 +0900

    Pawn 구현 완료
    [verifyMovePosition]
    - target과 source의 x값과 y값을 뺀 뒤, 둘의 거리를 구한다.
    - findDirection()의 매개변수로 둘의 거리값을 넣어 한 칸 전진 유형인지 대각 전진 유형인지 확인한다.
    - 만약 findDirection()이 null을 반환한다면 두 칸 이동 유형이거나 세 가지 유형을 벗어나는 예외일 것이다.
    - isTwoStep()을 통해 두 칸 이동 유형인지 아니면 그 외의 예외인지 판별한다.
    - 두 칸 이동 유형이면 이후 canMoveTwoStep()을 실행하여 두 칸을 갈 수 있는지 없는지 판별한다.
    - findDirection()에서 null이 아닌 Direction 값을 반환했다면 canMoveOnStep()을 실행한다.
    - 해당 메서드에서는 한 칸 전진일 경우와 대각 전진인 경우를 구분하여 값을 반환한다.

--- app/src/main/java/com/codesquad/chess/piece/Pawn.java
+++ app/src/main/java/com/codesquad/chess/piece/Pawn.java
@@ -6,35 +6,86 @@ import com.codesquad.chess.Position;
 
 public class Pawn extends Piece {
 
-    private boolean wasMoved;
-    public static int ONE_STEP_X_VAL = 0;
-    public static int TWO_STEP_Y_VAL = 2;
-    public static int WHITE_PAWN_START_Y_IDX = 6;
+    public static int SEC_RANK_IDX = 6;
+    public static int SEVENTH_RANK_IDX = 1;
+    public static int ONE_STEP_FORWARD = 1;
+    public static int TWO_STEP_FORWARD = 2;
 
     public Pawn(Piece.Color color, char representation, double point, Position position){
         super(color, representation, point, position);
-        wasMoved = false;
     }
 
     @Override
     public boolean verifyMovePosition(Position target, Board board){
-        int xMinus = this.getPosition().getX() - target.getX();
+        int xMinus = target.getX() - this.getPosition().getX(); // 이후 값이 -1이면 왼대각 이동, 1이면 오대각 이동
         int yMinus = this.getPosition().getY() - target.getY();
 
-        // 한 칸 전진 혹은 대각 전진 확인 --> 더 세분화 필요
-        for(Direction d : Direction.whitePawnDirection()){
-            if(d.getXDegree() == xMinus && d.getYDegree() == yMinus){
-                return true;
+        // 한 칸 전진 혹은 대각 전진 확인
+        Direction direction = findDirection(xMinus, yMinus);
+
+        // 두 칸 이동 로직
+        if(direction == null){
+            if(!isTwoStep(xMinus, yMinus))
+                return false;
+
+            return canMoveTwoStep(target, board);
+        }
+
+        // 전진 혹은 대각 이동 로직
+        return canMoveOneStep(direction, target, board);
+    }
+    private Direction findDirection(int x, int y){
+        if(this.getColor() == Color.WHITE){
+            for(Direction direction : Direction.whitePawnDirection()){
+                if(direction.getXDegree() == x && direction.getYDegree() == y){
+                    return direction;
+                }
+            }
+        } else if(this.getColor() == Color.BLACK){
+            for(Direction direction : Direction.blackPawnDirection()){
+                if(direction.getXDegree() == x && direction.getYDegree() == y){
+                    return direction;
+                }
             }
         }
-        
-        // 두 칸 전진 로직 확인
-        if(xMinus == ONE_STEP_X_VAL && yMinus == TWO_STEP_Y_VAL
-                && !wasMoved && this.getPosition().getY() == WHITE_PAWN_START_Y_IDX){
-            wasMoved = true;
-            return true;
+
+        return null;
+    }
+    private boolean canMoveTwoStep(Position target, Board board){
+        // 현재 위치가 백이면 rank 2 or 흑이면 rank 7 에 위치
+        Color srcColor = this.getColor();
+        Position srcPos = this.getPosition();
+
+        if(srcColor == Color.WHITE && srcPos.getY() == SEC_RANK_IDX){
+            Type oneStepType = board.findPiece(Position.of(srcPos.getX(), srcPos.getY() - ONE_STEP_FORWARD)).getType();
+            Type twoStepType = board.findPiece(Position.of(srcPos.getX(), srcPos.getY() - TWO_STEP_FORWARD)).getType();
+            return oneStepType == Type.NO_PIECE && twoStepType == Type.NO_PIECE;
+        }
+
+        if(srcColor == Color.BLACK && srcPos.getY() == SEVENTH_RANK_IDX){
+            Type oneStepType = board.findPiece(Position.of(srcPos.getX(), srcPos.getY() + ONE_STEP_FORWARD)).getType();
+            Type twoStepType = board.findPiece(Position.of(srcPos.getX(), srcPos.getY() + TWO_STEP_FORWARD)).getType();
+            return oneStepType == Type.NO_PIECE && twoStepType == Type.NO_PIECE;
         }
 
         return false;
+    }
+    private boolean isTwoStep(int xMinus, int yMinus){
+        // 남 혹은 북으로 두 칸 이동에 속하지 않으면 false
+        Position distance = Position.of(xMinus, yMinus);
+        Position nn = Position.of(Direction.NN.getXDegree(), Direction.NN.getYDegree());
+        Position ss = Position.of(Direction.SS.getXDegree(), Direction.SS.getYDegree());
+        return nn.equals(distance) || ss.equals(distance);
+    }
+    private boolean canMoveOneStep(Direction direction, Position target, Board board){
+        Color targetColor = board.findPiece(target).getColor();
+
+        // 한 칸 전진일 경우 -> 목표 지점이 Blank 객체일 때만 이동
+        if(direction == Direction.NORTH || direction == Direction.SOUTH){
+            return targetColor == Color.NOCOLOR;
+        }
+
+        // 대각일 경우 -> 목표 지점이 적군일 때만 이동
+        return targetColor != Color.NOCOLOR && targetColor != this.getColor();
     }
 }
--- app/src/test/java/com/codesquad/chess/piece/PawnTest.java
+++ app/src/test/java/com/codesquad/chess/piece/PawnTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class PawnTest {
 
@@ -19,29 +20,80 @@ public class PawnTest {
     }
 
     @Test
-    @DisplayName("흰색폰은 처음 한 칸 혹은 두 칸 북쪽으로 전진할 수 있다.")
-    public void verifyMovePosition_At_First_WhitePawn_Can_Move_OneOrTwoSpace(){
-        board.initializeEmptyBoard();
+    @DisplayName("폰은 처음 한 칸 혹은 두 칸 북쪽으로 전진할 수 있다.")
+    public void verifyMovePosition_InitialPosition_CanMoveOneOrTwoSquares(){
+        board.initialize();
 
-        Piece whitePawn1 = Piece.createWhitePawn(Position.of("d2"));
-        board.setPiece(Position.of("d2"), whitePawn1);
-        Piece whitePawn2 = Piece.createWhitePawn(Position.of("e2"));
-        board.setPiece(Position.of("e2"), whitePawn2);
-        System.out.println(ChessView.showBoard(board.getRanks()));
-
+        // 초기 설정
+        Piece whitePawn = Piece.createWhitePawn(Position.of("d2"));
+        Piece blackPawn = Piece.createBlackPawn(Position.of("e2"));
 
         // 첫 이동 시 한칸 혹은 두 칸 이동할 수 있음
+        // 흰색 폰 테스트
         board.move("d2", "d3"); // 한 칸 이동
         board.move("e2", "e4"); // 두 칸 이동
-        assertEquals(whitePawn1, board.findPiece(Position.of("d3")));
-        assertEquals(whitePawn2, board.findPiece(Position.of("e4")));
+        assertEquals(whitePawn, board.findPiece(Position.of("d3")));
+        assertEquals(whitePawn, board.findPiece(Position.of("e4")));
+        // 검정 폰 테스트
+        board.move("f7", "f6"); // 한 칸 이동
+        board.move("g7", "g5"); // 두 칸 이동
+        assertEquals(blackPawn, board.findPiece(Position.of("f6")));
+        assertEquals(blackPawn, board.findPiece(Position.of("g5")));
         System.out.println(ChessView.showBoard(board.getRanks()));
 
         // 첫 이동 후 한 칸만 전진할 수 있다.
+        // 흰색 폰 테스트
         board.move("d3", "d4"); // 한 칸 이동은 가능
         board.move("e4", "e6"); // 두 칸 이동은 불가
-        assertEquals(whitePawn1, board.findPiece(Position.of("d4")));
-        assertEquals(whitePawn2, board.findPiece(Position.of("e4"))); // 이동이 거부된 상태
+        assertEquals(whitePawn, board.findPiece(Position.of("d4")));
+        assertEquals(whitePawn, board.findPiece(Position.of("e4"))); // 이동이 거부된 상태
         System.out.println(ChessView.showBoard(board.getRanks()));
+        // 검정 폰 테스트
+        board.move("f6", "f5"); // 한 칸 이동은 가능
+        board.move("g5", "g3"); // 두 칸 이동은 불가
+        assertEquals(blackPawn, board.findPiece(Position.of("f5")));
+        assertEquals(blackPawn, board.findPiece(Position.of("g5"))); // 이동이 거부된 상태
+        System.out.println(ChessView.showBoard(board.getRanks()));
+    }
+
+    @Test
+    @DisplayName("폰은 적 기물이 대각선 방향에 있어야만 대각 이동이 가능하다.")
+    public void verifyMovePosition_DiagonalCapture_ReturnsTrue(){
+        board.initialize();
+
+        Piece whitePawn = Piece.createWhitePawn(Position.of("d6"));
+        Piece blackPawn = Piece.createBlackPawn(Position.of("e3"));
+        board.move("d2", "d4"); // 한칸 전진하고 실패한 뒤 잡는 역할
+        board.move("e2", "e4");
+        board.move("d7", "d5"); // 한칸 전진하고 실패한 뒤 잡는 역할
+        board.move("e7", "e5");
+
+        // d4의 whitePawn 한 칸 전진 후 실패, 이후 대각으로 전진하여 적 기물을 잡는다.
+        board.move("d4", "d5");
+        assertEquals(whitePawn, board.findPiece(Position.of("d4"))); // 실패
+        board.move("d4", "e5");
+        assertEquals(whitePawn, board.findPiece(Position.of("e5"))); // 성공
+
+        // d7의 blackPawn 한 칸 전진 후 실패, 이후 대각으로 전진하여 적 기물을 잡는다.
+        board.setPiece(Position.of("d4"), whitePawn);
+        board.move("d5", "d4");
+        assertEquals(blackPawn, board.findPiece(Position.of("d5"))); // 실패
+        board.move("d5", "e4");
+        assertEquals(blackPawn, board.findPiece(Position.of("e4"))); // 성공
+    }
+
+    @Test
+    @DisplayName("폰이 두 칸 이동 시 적을 뛰어넘어서는 안 된다")
+    public void verifyMovePosition_InFrontOfPiece_ReturnsFalse(){
+        board.initialize();
+
+        Position pos = Position.of("e3");
+        Piece whitePawn = Piece.createWhitePawn(pos);
+        board.setPiece(pos, whitePawn);
+        System.out.println(ChessView.showBoard(board.getRanks()));
+
+        board.move("e2", "e4");
+        assertEquals(whitePawn, board.findPiece(Position.of("e2")));
+        assertFalse(board.findPiece(Position.of("e4")).equals(whitePawn));
     }
 }
